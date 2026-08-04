623ee0fab5e489eca0a0b48217b18e51c7b23606
satish949498 <sk6288651@gmail.com>
2026-08-04 11:55:25 +0530

    Update API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expense-tracker-backend-y4nu.onrender.com/api",
 });
 
 // Automatically send JWT Token
